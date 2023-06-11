8978e8fbe17512e0ad02041bec7088e4b02e5106
Teona <teonazaalishvili2@gmail.com>
2023-06-11 12:45:45 -0400

    Installing and Configuring Flask-SQLAlchemy

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask 
+from flask_migrate import Migrate
 
 def create_app(): 
     app = Flask(__name__)
 
+    app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:Teona1990@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app,models.db)
+
     @app.route('/')
     def hello(): 
-        return 'Hello, PetFax!'
+     return 'Hello, PetFax!'
 
 
     from . import pet
